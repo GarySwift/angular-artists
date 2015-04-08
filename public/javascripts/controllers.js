cf89eaba9116e5a0bfdcdfb29147272b335a9884
Gary Swift <garyswiftmail@gmail.com>
2015-04-08 13:14:16 +0100

    getting artists from MongoDB

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -3,4 +3,8 @@ var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
 	$scope.mainTitle = "Angular Artists";
 	console.log('Hello From Controller');
+
+	$http.get('/artistlist').success( function (response) {
+		$scope.artists = response;
+	});		
 }]);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('artistlist', ['artistlist']);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Node Express & Angular' });
+  // res.render('index', { title: 'Node Express & Angular' });
+  res.use(express.static(__dirname + "/public"));
+});
+
+
+router.get('/artistlist', function (req, res) {
+    db.artistlist.find( function(err, docs) {
+        console.log(docs);
+        res.json(docs);
+    });
 });
 
 module.exports = router;
